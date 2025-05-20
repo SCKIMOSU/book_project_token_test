05858be0d2c822b5b74115fc97f3da8215e7b122
Sang-Chul Kim <sckim7@kookmin.ac.kr>
2025-05-20 17:26:06 +0900

    Add files via upload

---
+++ users/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+    def ready(self):
+        import users.signals  # 신호 연결 (예: Token 자동 생성)
+
+
